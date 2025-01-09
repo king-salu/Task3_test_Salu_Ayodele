fd6b2b5c043934b94f0e71389e0bdfdcf7a69ae4
kingsalu-probe <johndele94@gmail.com>
2025-01-09 05:51:19 +0100

    mobile adjustments

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,17 @@
-<div id="app"></div>
-@viteReactRefresh
-@vite('resources/js/app.jsx')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Twitter Portal</title>
+</head>
+
+<body>
+    <div id="app"></div>
+    @viteReactRefresh
+    @vite('resources/js/app.jsx')
+</body>
+
+</html>
